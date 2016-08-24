b3bf894dad49de43eba7e9116aca56c4d389a5fa
Alexey_Perov <perov-alexey@yandex.ru>
2016-08-24 13:34:03 +0300

    Add dependencies on page specific containers

--- contentscript.js
+++ contentscript.js
@@ -1,32 +1,51 @@
 var cntrlIsPressed = false;
-var canvas;
 
-document.body.addEventListener("keydown", function(event) {
-    if(event.which=="18") {
-        cntrlIsPressed = true;
+function keyDownHandler(event) {
+    if(event.which=="18") cntrlIsPressed = true;
+}
+
+function keyUpHandler(event) {
+    if(event.which=="18") cntrlIsPressed = false;
+}
+
+function initIframeListeners() {
+    var iframe = document.getElementById("ad");
+    if (iframe) {
+        iframe.contentWindow.document.body.addEventListener("keydown", keyDownHandler);
+        iframe.contentWindow.document.body.addEventListener("keyup", keyUpHandler);
+        iframe.contentWindow.document.body.addEventListener("click", takeScreenshot);
     }
-});
+}
 
-document.body.addEventListener("keyup", function(event) {
-    if(event.which=="18") {
-        cntrlIsPressed = false;
-    }
-});
-
-document.body.addEventListener("click", function() {
+function takeScreenshot(event) {
     if (cntrlIsPressed) {
+        var iframe = document.getElementById("ad");
+        var boundingRect = iframe.getBoundingClientRect();
         chrome.extension.sendMessage({
             dimensions: {
-                top: 0,
-                left: 0,
-                width: 100,
-                height: 100
+                top: boundingRect.top,
+                left: boundingRect.left,
+                width: boundingRect.right - boundingRect.left,
+                height: boundingRect.bottom - boundingRect.top
             }
         }, function(dataURL) {
-            document.getElementById("ad").dispatchEvent(new CustomEvent("screenshotTaken", {
-                "dataURL": dataURL
+            iframe.dispatchEvent(new CustomEvent("screenshotTaken", {
+                "detail": {
+                    "dataURL" : dataURL,
+                    "clientX" : event.clientX,
+                    "clientY" : event.clientY
+                }
             }));
         });
     }
+}
 
+document.addEventListener("DOMContentLoaded", function() {
+    document.body.addEventListener("keydown", keyDownHandler);
+    document.body.addEventListener("keyup", keyUpHandler);
+    document.body.addEventListener("iframeLoaded", initIframeListeners);
 });
+
+
+
+
