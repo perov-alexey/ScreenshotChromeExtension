2182e09bf2ed0e8df4f48bfad256278e271c3bc5
Alexey_Perov <perov-alexey@yandex.ru>
2016-08-25 17:59:58 +0300

    Add checking extension installation mechanism, fix problems

--- contentscript.js
+++ contentscript.js
@@ -1,49 +1,31 @@
-var cntrlIsPressed = false;
-
-function keyDownHandler(event) {
-    if(event.which=="18") cntrlIsPressed = true;
-}
-
-function keyUpHandler(event) {
-    if(event.which=="18") cntrlIsPressed = false;
-}
-
-function initIframeListeners() {
+function takeScreenshot() {
     var iframe = document.getElementById("ad");
-    if (iframe) {
-        iframe.contentWindow.document.body.addEventListener("keydown", keyDownHandler);
-        iframe.contentWindow.document.body.addEventListener("keyup", keyUpHandler);
-        iframe.contentWindow.document.body.addEventListener("click", takeScreenshot);
-    }
-}
-
-function takeScreenshot(event) {
-    if (cntrlIsPressed) {
-        var iframe = document.getElementById("ad");
-        var boundingRect = iframe.getBoundingClientRect();
-        chrome.extension.sendMessage({
-            dimensions: {
-                top: boundingRect.top,
-                left: boundingRect.left,
-                width: boundingRect.right - boundingRect.left,
-                height: boundingRect.bottom - boundingRect.top
-            }
-        }, function(dataURL) {
-            iframe.dispatchEvent(new CustomEvent("screenshotTaken", {
-                "detail": {
-                    "dataURL" : dataURL,
-                    "clientX" : event.clientX,
-                    "clientY" : event.clientY
-                }
-            }));
-        });
-    }
+    var boundingRect = iframe.getBoundingClientRect();
+    chrome.extension.sendMessage({
+        dimensions: {
+            top: boundingRect.top,
+            left: boundingRect.left,
+            width: boundingRect.right - boundingRect.left,
+            height: boundingRect.bottom - boundingRect.top
+        }
+    }, function (dataURL) {
+        iframe.dispatchEvent(new CustomEvent("screenshotTaken", {
+            "detail": dataURL
+        }));
+    });
 }
 
 document.addEventListener("DOMContentLoaded", function() {
-    document.body.addEventListener("keydown", keyDownHandler);
-    document.body.addEventListener("keyup", keyUpHandler);
-    document.body.addEventListener("iframeLoaded", initIframeListeners);
+    // For extension existence check
+    var indicator = document.createElement("div");
+    indicator.dataset.extensionid = "lbpcakpdeaognemhbgccjgihlmbmahmk";
+    document.body.appendChild(indicator);
+
+    document.body.addEventListener("iframeLoaded", function() {
+        document.getElementById("HTMLAdTest-pause").addEventListener("click", function() {
+            takeScreenshot();
+        })
+    });
 });
 
 
